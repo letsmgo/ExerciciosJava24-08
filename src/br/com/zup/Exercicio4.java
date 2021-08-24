aeb006dccd93c50c42a855f758d536e67d2dc370
Let Marçal <leticiamarcalmatias@gmail.com>
2021-08-24 16:02:11 -0300

    Exercício 4

---
+++ src/br/com/zup/Exercicio4.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        float qtdDeHorasTrabalhadas = 30;
+        float salarioPorHora = 150.9F;
+        float salarioBruto = qtdDeHorasTrabalhadas * salarioPorHora;
+
+        System.out.println("Você trabalhou " + qtdDeHorasTrabalhadas + " horas este mês, " + " e o salário bruto este mês é: R$" + salarioBruto);
+    }
+}
