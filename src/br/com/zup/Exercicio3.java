c77386d7938db91445cc0445420674f227f72a5a
Let Marçal <leticiamarcalmatias@gmail.com>
2021-08-24 15:58:05 -0300

    Exercício 3

---
+++ src/br/com/zup/Exercicio3.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+        //Para convertermos metros para centímetros, precisamos multiplicar o número representado em metros por 100.
+
+        float metro1 = 10;
+        float centimetro1 = 100;
+        float resultado = metro1 * centimetro1;
+
+        System.out.println("O total de centímetros em 10 metros é: " + resultado);
+    }
+}
