037bdd7d70595e9556e4a59951d4580d55637f44
Let Marçal <leticiamarcalmatias@gmail.com>
2021-08-25 14:06:11 -0300

    Introduzindo Scanner para inputar informações

--- src/br/com/zup/Exercicio2.java
+++ src/br/com/zup/Exercicio2.java
@@ -1,17 +1,36 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
     public static void main(String[] args) {
 
-        float primeiroBimestre = 10;
-        float segundoBimestre = 8.8F;
-        float terceiroBimestre = 9.4F;
-        float quartoBimestre = 10;
-        float totalDeNotasAvaliadas = 4;
-        float totalDosBimestres = primeiroBimestre + segundoBimestre + terceiroBimestre + quartoBimestre;
-        float mediaAritmetica = totalDosBimestres / totalDeNotasAvaliadas;
+        Scanner notas = new Scanner(System.in);
 
-        System.out.println("A média das notas do ano letivo foi: " + mediaAritmetica);
+
+
+        double nota1;
+        double nota2;
+        double nota3;
+        double nota4;
+        double soma;
+        double media;
+
+        System.out.println("Vamos calcular a média das suas notas?");
+        System.out.println("Por favor, digite a primeira nota:");
+        nota1 = notas.nextDouble();
+        System.out.println("Por favor, digite a segunda nota:");
+        nota2 = notas.nextDouble();
+        System.out.println("Por favor, digite a terceira nota:");
+        nota3 = notas.nextDouble();
+        System.out.println("Por favor, digite a quarta nota:");
+        nota4 = notas.nextDouble();
+
+        soma = nota1 + nota2 + nota3 + nota4;
+        media = soma / 4;
+
+        System.out.println("De acordo com as informações que vc digitou sua média final é: " + media);
+
 
     }
 }
