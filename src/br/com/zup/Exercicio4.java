5b30511261f802b931880006dea88b994dcc7e51
Let Marçal <leticiamarcalmatias@gmail.com>
2021-08-25 14:27:08 -0300

    Introduzindo Scanner para inputar dados

--- src/br/com/zup/Exercicio4.java
+++ src/br/com/zup/Exercicio4.java
@@ -1,11 +1,23 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
     public static void main(String[] args) {
-        float qtdDeHorasTrabalhadas = 30;
-        float salarioPorHora = 150.9F;
-        float salarioBruto = qtdDeHorasTrabalhadas * salarioPorHora;
 
-        System.out.println("Você trabalhou " + qtdDeHorasTrabalhadas + " horas este mês, " + " e o salário bruto este mês é: R$" + salarioBruto);
+        Scanner horasTrabalhadas = new Scanner(System.in);
+        Scanner salarioHora = new Scanner(System.in);
+        double horasMes;
+
+        System.out.println("Olá! Sou seu assistente para cálculo salarial");
+        System.out.println("Digite por gentileza quantas horas foram trabalhadas neste mês");
+        horasMes = horasTrabalhadas.nextDouble();
+        System.out.println("Ok, agora preciso que você digite o valor que você ganha por hora");
+        double salarioPorHora;
+        salarioPorHora = salarioHora.nextDouble();
+        System.out.println("Computei suas informações, e baseado nelas, seu salário será:");
+        double salarioBruto = horasMes * salarioPorHora;
+        System.out.println("R$" + salarioBruto);
+
     }
 }
