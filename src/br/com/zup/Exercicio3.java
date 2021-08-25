2e6369c3bd5f64a7852a72b24a9caf0eca292aa7
Let Marçal <leticiamarcalmatias@gmail.com>
2021-08-25 14:17:59 -0300

    Introduzindo Scanner para inputar dados!

--- src/br/com/zup/Exercicio3.java
+++ src/br/com/zup/Exercicio3.java
@@ -1,14 +1,24 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
     public static void main(String[] args) {
 
+        Scanner metroDigitado = new Scanner(System.in);
+
         //Para convertermos metros para centímetros, precisamos multiplicar o número representado em metros por 100.
 
-        float metro1 = 10;
-        float centimetro1 = 100;
-        float resultado = metro1 * centimetro1;
+        double metro1;
+        double centimetro = 100;
 
-        System.out.println("O total de centímetros em 10 metros é: " + resultado);
+        System.out.println("Olá, vamos calcular quantos centímetro existe em metros variados?");
+        System.out.println("Digite uma metragem aleatória:");
+        metro1 = metroDigitado.nextDouble();
+
+        double resultado = metro1 * centimetro;
+
+        System.out.println("A quantidade de centímetros é:");
+        System.out.println(resultado);
     }
 }
